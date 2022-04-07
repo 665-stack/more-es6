ce58a9c5224c76d5b994ef6550a80be5a9dd0546
unknown <mahmodulshahin665@gmail.com>
2022-04-07 22:34:33 +0600

    recap something

--- optional-chaining.js
+++ optional-chaining.js
@@ -6,5 +6,24 @@ const myObject = {
     a: 25,
     b: 68
 }
-const { x } = myObject;
-console.log(x)
+const { x, b } = myObject;
+console.log(x, b);
+// destructuring array; array er jonno [] bracket deya lage, var call korar 1st er ta first value, 2nd er ta second, array er call kora borabor shogkha nibe
+const array = [45, 47]
+const [p, q] = array;
+console.log(p, q)
+
+
+
+
+
+
+
+//========================
+//position wise milbe
+// array
+const friends = ['jonayed', 'akash']
+const [best, faltu] = friends;
+console.log(best, faltu)
+//object
+const { } = { sky: 'blue', soil: 'matti', color: 'red', monoy: 500 };
